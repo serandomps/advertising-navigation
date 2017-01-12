b9fbdceabab08e6e4dabe8422a39d139e2bc9e7b
Ruchira Wageesha <ruchira.wageesha@gmail.com>
2017-01-12 21:24:26 +0530

    updating

---
+++ index.js
@@ -0,0 +1,43 @@
+var serand = require('serand');
+var navigation = require('navigation');
+var utils = require('utils');
+
+var context;
+
+var ready = false;
+
+var render = function (done) {
+    $.ajax({
+        url: utils.resolve('accounts://apis/v/menus/1'),
+        dataType: 'json',
+        success: function (links) {
+            done(false, links);
+        },
+        error: function () {
+            done(true, {});
+        }
+    });
+};
+
+module.exports = function (sandbox, fn, options) {
+    context = {
+        sandbox: sandbox,
+        fn: fn
+    };
+    if (!ready) {
+        return;
+    }
+    render(function(err, links) {
+        navigation(sandbox, fn, links);
+    });
+};
+
+serand.on('user', 'ready', function (user) {
+    ready = true;
+    if (!context) {
+        return;
+    }
+    render(function(err, links) {
+        navigation(context.sandbox, context.fn, links);
+    });
+});
